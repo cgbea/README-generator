fc187b432138081940c99b6693f0fe304f790dc8
cgbea <151942177+cgbea@users.noreply.github.com>
2024-02-19 23:19:06 +0000

    Add license types

--- index.js
+++ index.js
@@ -9,39 +9,39 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'What is your project called?',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'description',
         message: 'Please describe your project',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'installation',
         message: 'What are your installation instructions?',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'usage',
         message: 'How should this project be used?',
-      },
-      {
+    },
+    {
         type: 'list',
         name: 'license',
         message: 'Choose the license for your project:',
         choices: ['MIT', 'Apache 2.0', 'GNU GPL v3', 'BSD 3-Clause', 'None'],
-    }
+    },
       {
         type: 'input',
         name: 'github',
         message: 'Enter your GitHub Username',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'email',
         message: 'Enter your email address',
-      },
-  
+    },
+
 ];
 
 // function to write README file
