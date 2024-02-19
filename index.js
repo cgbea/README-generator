947224450c7b86f0df3669cbcf0b9d51b37a829a
cgbea <151942177+cgbea@users.noreply.github.com>
2024-02-19 23:04:49 +0000

    generateMarkdown.js

--- index.js
+++ index.js
@@ -5,16 +5,60 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project called?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are your installation instructions?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How should this project be used?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose the license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GNU GPL v3', 'BSD 3-Clause', 'None'],
+    }
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address',
+      },
+  
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(fileName, data);
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+        .then((answers) => {
+            const markdown = generateMarkdown(answers);
+            writeToFile("README.md", markdown);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,36 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseWording = {
+    'MIT': `Explanatory text for MIT license goes here.`,
+    'Apache 2.0': `Explanatory text for Apache 2.0 license goes here.`,
+    'GNU GPL v3': `Explanatory text for GNU GPL v3 license goes here.`,
+    'BSD 3-Clause': `Explanatory text for BSD 3-Clause license goes here.`,
+    'None': `No license chosen.`,
+};
   return `# ${data.title}
 
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+### ${data.license}
+${licenseWording[data.license]}
+
+## Questions
+To learn more about me, or for any questions, feel free to contact me through GitHub [${data.github}](https://github.com/${data.github}), or email ${data.email}.
 `;
 }
 
